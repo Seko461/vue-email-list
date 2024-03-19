ce498bb846737d7bd38ec9fc8e64c5b228a07195
Seko461 <sekopierre@gmail.com>
2024-03-19 13:53:51 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            randomMail: null,
+        }
+    },
+    methods: {
+        callApi() {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                    console.log(response.data.response);
+                    this.randomMail = response.data.response
+                })
+        },
+
+    },
+    mounted() {
+        this.callApi()
+
+    }
+}).mount('#app')
