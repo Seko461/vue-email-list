6774cef8c1fb9bcfcd4bcae48a17b9fac04d4f4b
Seko461 <sekopierre@gmail.com>
2024-03-21 00:00:34 +0100

    fixed

--- app.js
+++ app.js
@@ -2,46 +2,49 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            api_url: "https://flynn.boolean.careers/exercises/api/random/mail",
             mail: null,
             mailList: [],
+            error: null,
 
 
         }
     },
     methods: {
-        generateMails() {
-
+        generateList() {
             for (let i = 0; i < 10; i++) {
                 axios
                     .get("https://flynn.boolean.careers/exercises/api/random/mail")
-                    .then((response) => {
+                    .then(response => {
 
-                        console.log(response.data.response);
-                        this.mail = response.data.response
-
-
-                        this.mailList.push(this.mail)
-
-                    });
+                        console.log(response);
+                        this.mailList.push(response.data.response);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        this.error = error
+                    })
 
             }
-
-            console.log(this.mailList);
-
         },
-        async generateMails() {
-
-
-            for (let i = 0; i < 10; i++) {
-                const response = await axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                this.mailList.push(response.data.response)
-            }
-
-        },
-
         deleteAll() {
-            location.reload()
-        },
+            location.reload();
+        }
     },
+    mounted() {
+        axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+
+                console.log(response);
+
+
+            });
+
+
+
+
+    }
+
 
 }).mount('#app')
